113ceb7a98e531b0c6f21eddb6c4055f67b8699b
Cesar <139836008+cesarf95@users.noreply.github.com>
2025-09-09 22:34:16 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,149 @@
+//Aula Terça Feira 09/09/2025
+
+
+// // // // // Criar uma variavel 'nome' com seu nome e mostrar 'Ola. <nome>!.'
+
+// // // // let nome = 'Cesar Fonseca';
+// // // // let idade = 20
+// // // // let cidade = 'Maringá'
+// // // // console.log(`Meu nome é ${nome}, tenho ${idade} anos, moro em ${cidade}`)
+
+// // // // // variavel nome, idade, cidade - 'Meu nome é 'nome', eu tenho 'idade' anos e moro em 'cidade'
+
+
+
+// // // // Imprimindo tipos de dados
+
+// // // const numero = 123;
+// // // const naoEhNumero = '123';
+// // // console.log(numero + naoEhNumero);
+// // // console.log(typeof numero);
+// // // console.log(typeof naoEhNumero);
+// // // console.log(typeof true);
+// // // console.log(typeof '');
+// // console.log(typeof 3.15);
+
+
+// const numero1 = 7;
+// const numero2 = '11';
+
+// console.log(numero1 == numero2);
+
+
+// const numero3 = 7;
+// const numero4 = '7';
+
+// console.log(numero3 == numero4);
+
+// const numero5 = 7;
+// const numero6 = '7';
+
+// console.log(numero5 === numero6);
+
+// // = recebe
+// // == igual
+// // === identico 
+
+//Crie 5 variaveis com tipos diferentes. Em seguida imprima no console os tipos delas.
+
+
+//  let nome = 'Cesar'
+//  const idade = 20
+//  let cidade = true
+//  var tipos ;
+//  var valor = 0
+//  let cesar = null
+// let frutas =["Uva", "Banana", "Morango"]
+
+//  console.log(typeof nome)
+//  console.log(typeof idade) 
+//  console.log(typeof cidade)
+//  console.log(typeof tipos)
+//  console.log(typeof valor)
+//  console.log(typeof cesar)
+//  console.log(typeof Mala)
+
+
+
+
+
+// const idade = 18;
+// if (idade >= 18) {
+//     console.log('E maior de idade')
+// } else {
+//     console.log('e menor de idade')
+// }
+
+// const idade = 14;
+// if (idade >= 16 && idade <= 18 || idade > 70) {
+//     console.log('Voto opcional')
+
+// } else if (idade >= 18 && idade <= 60)
+// { console.log("E obrigado a votar")
+
+// } else
+//     console.log("Nao pode votar")
+
+
+// const idade = 70;
+// if (idade <16){
+//     console.log('nao pode votar')
+// } else if (idade >=16 && idade <88){
+//     console.log('voto facultativo')
+// } else {
+//     console.log('obrigatorio')
+// }
+
+
+// const numero = 13
+// undefined
+// if (numero % 2 === 0) {cons}
+// undefined
+// if (numero % 2 === 0) {console.log('É par')} else
+// {console.log('É impar')}
+
+
+//Criar variavel com valor, Condição se o valor > 100 desconto de 10% senao o desconto sera de 5%. Valor final com 2 casas decimais
+
+
+
+
+// const valor = 120;
+// if(valor > 100){
+//     desconto = valor * 0.10;
+
+// console.log("O desconto sera de 10%")
+// } else {
+
+//  if (valor <= 100)
+
+//  desconto = valor * 0.05;
+
+// console.log("O desconto sera de 5%")
+// }
+// console.log(`O desconto é de: R$ ${desconto.toFixed(2)}`)
+
+//Criar variavel mostrando o dia 3. Mostre dom=1 seg=2 ...  
+
+
+// const dia = 4;
+// let nomeDia;
+// switch(dia) {
+//     case 1: nomeDia = "Domingo";
+//     break;
+//     case 2: nomeDia = "Segunda";
+//     break;
+//     case 3: nomeDia = "Terca";
+//     break;
+//     case 4: nomeDia = "Quarta";
+//     break;
+//     case 5: nomeDia = "Quinta";
+//     break;
+//     case 6: nomeDia = "Sexta";
+//     break;
+//     case 7: nomeDia = "Sabado";
+//     break;
+//     default: nomeDia = "Dia invalido"
+    
+// }
+// console.log(nomeDia)
